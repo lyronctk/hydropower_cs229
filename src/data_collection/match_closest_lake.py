3b7ee649a63fa2b545317a50595b77b760a95025
lyronctk <lyronctk@gmail.com>
2020-10-23 11:28:33 -0700

    rest

---
+++ src/data_collection/match_closest_lake.py
@@ -0,0 +1,17 @@
+import csv
+
+DATA_DIR = '../../data'
+LAKES_TARGET_CSV = f'{DATA_DIR}/lakes_2015.csv'
+BASE_CSV = f'{DATA_DIR}/base_dataset.csv'
+
+def load_lakes():
+    with open(LAKES_TARGET_CSV)
+
+
+def main():
+    print('hello world')
+    lakes = load_lakes()
+
+
+if __name__ == '__main__':
+    main()
